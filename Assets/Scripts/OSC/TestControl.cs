35a60c84ed2feeae2d259fe9c41afc61f8a08617
Shuaikx <shuaikx828@gmail.com>
2025-06-26 04:35:08 +0800

    Meet bugs in OSC Test

---
+++ Assets/Scripts/OSC/TestControl.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TestControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void SetScale(float newScale)
+    {
+        // 将接收到的单个浮点数值同时应用到x, y, z轴上
+        transform.localScale = new Vector3(newScale/10, newScale/10, newScale/10);
+    }
+}
