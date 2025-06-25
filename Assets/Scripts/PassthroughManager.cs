fec4cc26d881805fa33ac1873203a546e737abe4
Shuaikx <shuaikx828@gmail.com>
2025-06-26 02:32:23 +0800

    fix some bus in URP

--- Assets/Scripts/PassthroughManager.cs
+++ Assets/Scripts/PassthroughManager.cs
@@ -1,80 +1,79 @@
-// using UnityEngine;
-
-// public class PassthroughManager : MonoBehaviour
-// {
-//     public Camera m_Camera;
-
-//     private void Start()
-//     {
-//         m_Camera.clearFlags = CameraClearFlags.Skybox;
-//         m_Camera.backgroundColor = Color.clear;
-//     }
-//     public void ToggleSkybox()
-//     {
-//         if (m_Camera.clearFlags == CameraClearFlags.Skybox)
-//         {
-//             m_Camera.clearFlags = CameraClearFlags.Color;
-//             m_Camera.backgroundColor = Color.clear;
-//         }
-//         else
-//         {
-//             m_Camera.clearFlags = CameraClearFlags.Skybox;
-
-//         }
-
-//     }
-// }
-
 using UnityEngine;
-using System.Collections; 
 
 public class PassthroughManager : MonoBehaviour
 {
-    [Tooltip("Main Camera")]
-    [SerializeField] private Camera m_Camera;
-
-    [SerializeField] private float fadeDuration = 1.0f;
-    [SerializeField] private Color backgroundColor = Color.black;
-
-    private bool isTransparent = true; 
-    private Coroutine activeFadeCoroutine;
+    public Camera m_Camera;
 
     private void Start()
     {
-        m_Camera.clearFlags = CameraClearFlags.Color;
-        m_Camera.backgroundColor = backgroundColor;
-        isTransparent = false;
+        m_Camera.clearFlags = CameraClearFlags.Skybox;
     }
-
-    public void TogglePassthrough() 
+    public void TogglePassthrough()
     {
-        isTransparent = !isTransparent;
-
-        Color targetColor = isTransparent ? Color.clear : backgroundColor;
-
-        if (activeFadeCoroutine != null)
+        if (m_Camera.clearFlags == CameraClearFlags.Skybox)
         {
-            StopCoroutine(activeFadeCoroutine);
+            m_Camera.clearFlags = CameraClearFlags.Color;
+            m_Camera.backgroundColor = Color.clear;
+        }
+        else
+        {
+            m_Camera.clearFlags = CameraClearFlags.Skybox;
+
         }
 
-        activeFadeCoroutine = StartCoroutine(FadeColorCoroutine(targetColor, fadeDuration));
-    }
-
-    private IEnumerator FadeColorCoroutine(Color targetColor, float duration)
-    {
-        Color startingColor = m_Camera.backgroundColor;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
-            m_Camera.backgroundColor = Color.Lerp(startingColor, targetColor, t);
-            yield return null;
-        }
-
-        m_Camera.backgroundColor = targetColor;
-        activeFadeCoroutine = null; 
     }
 }
+
+// using UnityEngine;
+// using System.Collections; 
+
+// public class PassthroughManager : MonoBehaviour
+// {
+//     [Tooltip("Main Camera")]
+//     [SerializeField] private Camera m_Camera;
+
+//     [SerializeField] private float fadeDuration = 1.0f;
+//     [SerializeField] private Color backgroundColor = Color.black;
+
+//     private bool isTransparent = true; 
+//     private Coroutine activeFadeCoroutine;
+
+//     private void Start()
+//     {
+//         m_Camera.clearFlags = CameraClearFlags.Color;
+//         m_Camera.backgroundColor = backgroundColor;
+//         isTransparent = false;
+//     }
+
+//     public void TogglePassthrough() 
+//     {
+//         isTransparent = !isTransparent;
+
+//         Color targetColor = isTransparent ? Color.clear : backgroundColor;
+
+//         if (activeFadeCoroutine != null)
+//         {
+//             StopCoroutine(activeFadeCoroutine);
+//         }
+
+//         activeFadeCoroutine = StartCoroutine(FadeColorCoroutine(targetColor, fadeDuration));
+//     }
+
+//     private IEnumerator FadeColorCoroutine(Color targetColor, float duration)
+//     {
+//         Color startingColor = m_Camera.backgroundColor;
+//         float elapsedTime = 0f;
+
+//         while (elapsedTime < duration)
+//         {
+//             elapsedTime += Time.deltaTime;
+//             float t = Mathf.Clamp01(elapsedTime / duration);
+//             m_Camera.backgroundColor = Color.Lerp(startingColor, targetColor, t);
+//             yield return null;
+//         }
+
+//         m_Camera.backgroundColor = targetColor;
+//         activeFadeCoroutine = null; 
+//     }
+// }
 
